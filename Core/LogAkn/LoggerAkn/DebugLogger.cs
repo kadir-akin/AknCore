9c55d635ca4660362a300967a8b5fc6ee30ed8b8
kadir-akin <yazilim.kadir.akin@gmail.com>
2023-11-30 02:19:23 +0300

    debug logger added

---
+++ Core/LogAkn/LoggerAkn/DebugLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.LogAkn.LoggerAkn
+{
+    public class DebugLogger : ILogger
+    {
+        IHostingEnvironment _hostingEnvironment;
+        public DebugLogger(IHostingEnvironment hostingEnvironment) => _hostingEnvironment = hostingEnvironment;
+        public IDisposable BeginScope<TState>(TState state) => null;
+        public bool IsEnabled(LogLevel logLevel) => true;
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, System.Exception exception, Func<TState, System.Exception, string> formatter)
+        {
+            Console.WriteLine($"Log Level : {logLevel.ToString()} | Event ID : {eventId.Id} | Event Name : {eventId.Name} | Formatter : {formatter(state, exception)}");
+
+        }
+    }
+}
+
