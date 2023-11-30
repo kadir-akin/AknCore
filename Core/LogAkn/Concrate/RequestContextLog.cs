30a8e09978e7930d5998894310b3aa8525e84f5b
kadir-akin <yazilim.kadir.akin@gmail.com>
2023-11-30 19:53:49 +0300

    RequestContextLog ctor

--- Core/LogAkn/Concrate/RequestContextLog.cs
+++ Core/LogAkn/Concrate/RequestContextLog.cs
@@ -5,6 +5,8 @@ using Core.Security.Abstract;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Core.LogAkn.Concrate
@@ -24,9 +26,23 @@ namespace Core.LogAkn.Concrate
         public string ActionPath { get; set; }
         public string LogLevel { get; set; }
 
-        public RequestContextLog(string message,HttpContext httpContext,AknException  aknException, IAknRequestContext requestContext, IAknUser user,string logLevel, ProjectInfoConfiguration projectInfo)
-        {
+        public string Query { get; set; }
 
+        public RequestContextLog( HttpContext httpContext, AknException aknException, IAknRequestContext requestContext, IAknUser user, string logLevel, ProjectInfoConfiguration projectInfo)
+        {
+            RequestContext = requestContext;
+            User = user;
+            ProjectName = projectInfo?.ProjectName;
+            ApplicationName = projectInfo?.ApplicationName;
+            StatusCode = aknException==null ? ((int)HttpStatusCode.OK) : (aknException.ExceptionDetailList.FirstOrDefault()?.Status ?? ((int)HttpStatusCode.OK) );
+            ReturnCode = aknException == null ? ((int)HttpStatusCode.OK) : (aknException.ExceptionDetailList.FirstOrDefault()?.Status ?? ((int)HttpStatusCode.OK));
+            Message = aknException.ExceptionDetailList.FirstOrDefault()?.Message;
+            CreateDate = DateTime.Now.ToString("yyyy'-'MM'-'dd''HH':'mm':'ss");
+            AknExceptionType = aknException.ExceptionDetailList.FirstOrDefault()?.AknExceptionType.ToString();
+            Exception = aknException.ExceptionDetailList.FirstOrDefault()?.Exception;
+            ActionPath = httpContext !=null ? httpContext.Request.Path : null;
+            LogLevel = logLevel;
+            Query = httpContext != null ? httpContext.Request.QueryString.Value : null;
         }
     }
 }
