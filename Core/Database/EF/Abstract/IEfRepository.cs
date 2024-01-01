614604719aa4f0f9aa100307394f208270ee81a9
kadir-akin <yazilim.kadir.akin@gmail.com>
2024-01-01 04:34:11 +0300

    fixing

--- Core/Database/EF/Abstract/IEfRepository.cs
+++ Core/Database/EF/Abstract/IEfRepository.cs
@@ -7,19 +7,14 @@ namespace Core.Database.EF.Abstract
 {
     public interface IEfRepository<TEntity> where TEntity : class,IEntity
     {
-        public  Task AddAsync(TEntity entity);
-
+        public Task<TEntity> AddAsync(TEntity entity);
         public Task AddRangeAsync(IEnumerable<TEntity> entities);
-
-        public IEnumerable<TEntity> GetListAsync(Expression<Func<TEntity, bool>> predicate);
-
-        public TEntity GetByIdAsync(int id);
-
+        public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate = null);
+        public Task<TEntity> GetByIdAsync(int id);
+        public Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate = null);
         public Task RemoveAsync(TEntity entity);
-
         public Task RemoveRangeAsync(IEnumerable<TEntity> entities);
+        
 
-        public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
-       
     }
 }
---
+++ Core/Database/EF/Concrate/EfRepository.cs
@@ -0,0 +1,68 @@
+﻿using Core.Database.EF.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Database.EF.Concrate
+{
+    internal class EfRepository<TEntity> : IEfRepository<TEntity> where TEntity : class, IEntity
+    { 
+        protected readonly DbContext Context;
+        public EfRepository(DbContext context)
+        {
+            Context = context;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            var result = await Context.Set<TEntity>().AddAsync(entity);
+            Context.SaveChanges();
+            return result.Entity;
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await Context.Set<TEntity>().AddRangeAsync(entities);
+        }
+
+        public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            if (predicate == null)
+            {
+                return Context.Set<TEntity>().FirstOrDefaultAsync();
+            }
+            return Context.Set<TEntity>().Where(predicate)?.FirstOrDefaultAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            if (predicate == null)
+            {
+                return await Context.Set<TEntity>().ToListAsync();
+            }
+            return await Context.Set<TEntity>().Where(predicate)?.ToListAsync();
+        }
+
+        public Task RemoveAsync(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+            Context.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+            Context.SaveChanges();
+            return Task.CompletedTask;
+        }
+    }
+}
